dab6be77c760de1083bc7730bf27952a43d2baca
Alexandre Atoji <alexandre.atoji@gmail.com>
2011-10-31 14:50:13 -0200

    including file

---
+++ src/com/beyondweb/pong/util/Constants.java
@@ -0,0 +1,9 @@
+package com.beyondweb.pong.util;
+
+public class Constants {
+
+	public static int PONG_REQUEST_CODE = 15;
+	public static int DIFFICULTY_EASY = 16;
+	public static int DIFFICULTY_MEDIUM = 12;
+	public static int DIFFICULTY_HARD = 8;
+}
